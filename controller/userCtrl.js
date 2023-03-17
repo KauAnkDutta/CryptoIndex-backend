35026a94cef97902173fd5d2d0d4fd409c2e1ad8
KauAnkDutta <dutta.kaushik1998@gmail.com>
2023-03-17 22:44:29 +0530

    degug

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -94,10 +94,11 @@ const userCtrl = {
                     httpOnly: true,
                     maxAge: 86400000, // 1 day in milliseconds
                     secure: true, // Set this to true if your website is hosted on HTTPS
-                    sameSite: 'none' // Set this to 'none' if you want to allow cross-site cookies
+                    sameSite: 'none', // Set this to 'none' if you want to allow cross-site cookies
+                    domain: "nrender.com"
                   });
                 // ---------------
-                
+
                 var mailOptions = {
                     from: process.env.USER,
                     to: email,
